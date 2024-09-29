c828892b0292d71fc550acf3ef35c875125aa2b0
somyonn <turfksm@gmail.com>
2024-09-29 22:54:14 +0900

    롬복 할 수 있게 만들기

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,4 @@ class JpashopApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
